7cee706628648f97b4e1b2c8d48d5b49c09705b5
Fernando Bahamondes <ferbaco@gmail.com>
2020-07-04 15:51:42 -0400

    Update CRUD methods in categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,38 @@
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.isvalid?
+      @category.save
+
+      redirect_to(root_path)
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
   end
 
   def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+
+    redirect_to(root_path)
+
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to(root_path)
+  end
+
+  private
+  def category_params
+    params.require(:categories).permit(:name, :priority)
   end
 end
