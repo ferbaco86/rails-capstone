abfb0e0dce2ef9fc6f88f4c1585208b8ded934f3
Fernando Bahamondes <ferbaco@gmail.com>
2020-07-10 14:13:47 -0400

    Add font path to application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module RailsCapstone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.paths << Rails.root.join("app","assets","fonts")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
