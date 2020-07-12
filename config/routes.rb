d98c7daad3b88b3959426f8d07574f09a99f28ca
Fernando Bahamondes <ferbaco@gmail.com>
2020-07-12 01:53:55 -0400

    Update routes to only allow methods used

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "categories#home" 
   resources :users
-  resources :categories
-  resources :sessions
-  resources :articles do
-    resources :votes
+  resources :categories, except: [:edit, :update, :destroy]
+  resources :sessions, except: [:destroy]
+  resources :articles, except: [:destroy] do
+    resources :votes, except: [:destroy]
   end
 end
