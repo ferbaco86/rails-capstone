f80d0dcbf3143459ae3100defd309b594f290fa2
Fernando Bahamondes <ferbaco@gmail.com>
2020-07-13 12:49:01 -0400

    Add uniqueness to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   has_many :article_categories
   has_many :articles, through: :article_categories
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :priority, presence: true
 
   scope :categories_priority, -> { order('priority DESC') }
